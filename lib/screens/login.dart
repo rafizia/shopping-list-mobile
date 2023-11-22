6616d7f5d4928fee41f09b355a182c16effcbc82
rafizia <mohrafiziaulhaq@gmail.com>
2023-11-22 14:29:02 +0700

    fix deployment

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -68,7 +68,7 @@ class _LoginPageState extends State<LoginPage> {
                 // Cek kredensial
                 // Untuk menyambungkan Android emulator dengan Django pada localhost,
                 // gunakan URL http://10.0.2.2/
-                final response = await request.login("http://localhost:8000/auth/login/", {
+                final response = await request.login("https://muhammad-rafi22-tutorial.pbp.cs.ui.ac.id/auth/login/", {
                   'username': username,
                   'password': password,
                 });
